f6ede685bdafe0e1c8f188ebdd1bc24bdcace679
Timur Nikitemko <timka.nikitenko@mail.ru>
2024-11-07 12:50:07 +0000

    Added class with declarations of methods for loading, rotating and saving BMP image

--- bmp.hpp
+++ bmp.hpp
@@ -1,36 +1,44 @@
 #ifndef BMP_HPP
-#define BMP_CPP
+#define BMP_HPP
 
 #include <cstdint>
 #include <string>
 #include <vector>
 
-#pragma pack(push, 1)
-
 struct BMPHeader {
-	uint16_t fileType{0x4D42};
-	uint32_t fileSize{0};
-	uint32_t reserved{0};
-	uint32_t offsetDate{0};
+	uint16_t fileType;
+	uint32_t fileSize;
+	uint32_t reserved1;
+	uint32_t reserved2;
+	uint32_t offsetData;
 };
 
 struct BMPInfoHeader {
-	uint32_t size{0};
-	int32_t width{0};
-	int32_t height{0};
-	uint16_t planes{1};
-	uint16_t bitCount{24};
-	uint32_t compression{0};
-	uint32_t sizeImage{0};
-	int32_t xPixelsPerMeter{0};
-	int32_t yPixelsPerMeter{0};
-	uint32_t colorsUsed{0};
-	uint32_t colorsImportant{0};
+	uint32_t size;
+	int32_t width;
+	int32_t height;
+	uint16_t planes;
+	uint16_t bitCount;
+	uint32_t compression;
+	uint32_t sizeImage;
+	int32_t xPixelsPerMeter;
+	int32_t yPixelsPerMeter;
+	uint32_t colorsUsed;
+	uint32_t colorsImportant;
 };
 
-#pragma pack(pop)
+class BMPImage {
+	public:
+		BMPHeader header;
+		BMPInfoHeader infoHeader;
+		std::vector<uint8_t> pixelData;
 
-bool loadBMP(const std::string &filename, BMPHeader &bmpHeader, BMPInfoHeader &bmpInfoHeader, std::vector<uint8_t> &imageData);
+		void load(const std::string &filename);
+		void save(const std::string &filename);
+		void rotateClockwise();
+		void rotateCounterclockwise();
+		void applyGaussianFilter();
+};
 
 #endif
 
