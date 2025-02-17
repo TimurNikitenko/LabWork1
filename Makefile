0b7284b50896aa6c060e94c700bcad26080a56fc
Timur Nikitemko <timka.nikitenko@mail.ru>
2025-02-17 14:56:58 +0000

    Updated labwork with with necessary corrections

--- Makefile
+++ Makefile
@@ -1,17 +1,18 @@
-start: main.o bmp.o
-        g++ main.o bmp.o -o start
 
-main.o: main.cpp
-        g++ -c main.cpp
+PROJECT = start
 
-bmp.o: bmp.cpp bmp.hpp
-        g++ -c bmp.cpp
+CXX = g++
+
+SRC = bmp.cpp
+
+OBJ = $(SRC:.cpp=.o)
+
+$(PROJECT): $(OBJ)
+	$(CXX) -o $(PROJECT) $(OBJ) $(GTEST_LIBS)
+
+
+%.o: %.cpp
+	$(CXX) -c $< -o $@
 
 clean:
-	rm *.o start
-
-
-
-
-
-
+	rm -f $(OBJ) $(PROJECT)
--- bmp.hpp
+++ bmp.hpp
@@ -1,48 +1,204 @@
-/* Timur Nikitenko st128375@student.spbu.ru
- *first labwork
- */
+/*Nikitenko Timur st128375@student.spbu.ru
+  BMP
+*/
+#ifndef BMP
+#define BMP
+#include<iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+#include<cmath>
+#include<utility>
 
-#ifndef BMP_HPP
-#define BMP_HPP
 
-#include <cstdint>
-#include <string>
-#include <vector>
+struct Image
+{
+public:
+    
+    int width;
+    int height;
+    std::string path;
 
-struct BMPHeader {
-	uint16_t fileType;
-	uint32_t fileSize;
-	uint32_t reserved1;
-	uint32_t reserved2;
-	uint32_t offsetData;
+    
+    Image(int width, int height, std::string path) : width(width), height(height), path(path)
+    {
+        const int imageSize = width * height;
+
+        
+    }
+    
+
+   
+    std::vector<unsigned char> readToVector(std::string path, int size)
+    {
+    
+        std::ifstream file(path, std::ios::binary);
+
+       
+        std::vector<unsigned char> output (size);
+
+      
+        if (!file.read(reinterpret_cast<char*>(output.data()), size))
+        {
+            throw std::runtime_error("Error reading file: " + path);
+        }
+        
+        return output;
+
+    }
+
+    void writeToRaw(std::string filename, std::vector<unsigned char> vector)
+    {
+        std::ofstream file(filename, std::ios::binary);
+        file.write(reinterpret_cast<const char*>(vector.data()), vector.size());
+        file.close();
+    }
+
+    std::vector<std::vector<unsigned char>> oneDimToTwoDim(std::vector <unsigned char> one_dim)
+    {
+        std::vector<std::vector<unsigned char>> two_dim(height, std::vector<unsigned char>(width));
+        for (int y = 0; y<height; y++)
+        {
+            for (int x = 0; x<width; x++)
+            {
+                two_dim[y][x] = one_dim[y * width + x];
+            }
+        }
+        return two_dim;
+    }
+
+    std::vector<unsigned char> twoDimToOneDim(std::vector<std::vector<unsigned char>> two_dim)
+    {
+        std::vector<unsigned char> one_dim(width*height);
+        for (int x = 0; x<width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                one_dim[x + y * width] = two_dim[y][x];
+            }
+        }
+        return one_dim;
+    }
+
+    std::vector<unsigned char> clockwiseRotate(std::vector<unsigned char> img)
+    {
+        auto img_2d = oneDimToTwoDim(img);
+        std::vector<std::vector<unsigned char>> rotated_img(width, std::vector<unsigned char>(height));
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                rotated_img[x][height - y - 1] = img_2d[y][x];
+            }
+        }
+
+        return twoDimToOneDim(rotated_img);
+    }
+
+
+    std::vector<unsigned char> counterClockwiseRotate(std::vector<unsigned char> img)
+    {
+        return clockwiseRotate(clockwiseRotate(clockwiseRotate(img)));
+    }
+
+    std::pair<std::vector<std::vector<float>>, int> makeKernel(int sigma)
+    {
+        int kernel_size = (sigma * 6) + 1;  
+        int kernel_length = kernel_size;    
+        std::vector<std::vector<float>> kernel(kernel_length, std::vector<float>(kernel_length));
+
+        float sum = 0.0f;
+
+       
+        int radius = kernel_length / 2;
+        for (int y = -radius; y <= radius; y++)
+        {
+            for (int x = -radius; x <= radius; x++)
+            {
+                
+                kernel[y + radius][x + radius] = (1.0f / (2.0f * 3.14f * sigma * sigma)) * std::exp(-((x * x + y * y) / (2.0f * sigma * sigma)));
+                sum += kernel[y + radius][x + radius];
+            }
+        }
+
+       
+        for (int y = 0; y < kernel_length; y++)
+        {
+            for (int x = 0; x < kernel_length; x++)
+            {
+                kernel[y][x] /= sum;
+            }
+        }
+
+        return {kernel, kernel_length};
+    }
+
+    template<typename T>
+    T clamp(T value, T min_value, T max_value)
+    {
+        if (value < min_value)
+        {
+            return min_value;
+        }
+        else if (value > max_value)
+        {
+            return max_value;
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    void saveAsGaussianImage(std::vector<unsigned char> img, int sigma)
+    {
+        auto img_2d = oneDimToTwoDim(img);
+
+        auto kernel_pair = makeKernel(sigma);
+        auto kernel = kernel_pair.first;
+        int kernel_length = kernel_pair.second;
+        int kernel_radius = kernel_length / 2;
+
+     
+        std::vector<std::vector<unsigned char>> blurred_img(height, std::vector<unsigned char>(width, 0));
+
+       
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sum = 0.0f;
+
+                for (int ky = -kernel_radius; ky <= kernel_radius; ky++)
+                {
+                    for (int kx = -kernel_radius; kx <= kernel_radius; kx++)
+                    {
+                        
+                        int ny = y + ky;
+                        int nx = x + kx;
+
+                        if (ny < 0) ny = 0;
+                        if (ny >= height) ny = height - 1;
+                        if (nx < 0) nx = 0;
+                        if (nx >= width) nx = width - 1;
+
+                        sum += img_2d[ny][nx] * kernel[ky + kernel_radius][kx + kernel_radius];
+                    }
+                }
+
+              
+                blurred_img[y][x] = static_cast<unsigned char>(clamp(std::round(sum), 0.0f, 255.0f));
+            }
+        }
+
+        
+        auto blurred_img_1d = twoDimToOneDim(blurred_img);
+
+       
+        writeToRaw("gaussian_blurred.raw", blurred_img_1d);
+    }
+
+
 };
-
-struct BMPInfoHeader {
-	uint32_t size;
-	int32_t width;
-	int32_t height;
-	uint16_t planes;
-	uint16_t bitCount;
-	uint32_t compression;
-	uint32_t sizeImage;
-	int32_t xPixelsPerMeter;
-	int32_t yPixelsPerMeter;
-	uint32_t colorsUsed;
-	uint32_t colorsImportant;
-};
-
-class BMPImage {
-	public:
-		BMPHeader header;
-		BMPInfoHeader infoHeader;
-		std::vector<uint8_t> pixelData;
-
-		void load(const std::string &filename);
-		void save(const std::string &filename);
-		void rotateClockwise();
-		void rotateCounterclockwise();
-		void applyGaussianFilter();
-};
-
 #endif
-
